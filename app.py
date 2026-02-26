235319c4cd484d5a93f42705eeb3f4f6101c2257
DanielServejeira <servejeiradaniel@gmail.com>
2026-02-26 18:14:35 -0300

    feat: carregar e baixar arquivos

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 from compiler.errors import LexerError
 from compiler.lexer import Lexer
 
 st.markdown("<h1 style='text-align: center;'>Compilador - Analisador Léxico</h1>", unsafe_allow_html=True)
+
+file_uploader = st.file_uploader(label="Carregar expressão matemática", max_upload_size=1, accept_multiple_files=False, width="stretch")
 
 sample = "(10 + 5.5) / 2 - .25"
 text_area = st.text_area("Expressão matemática:", value=sample, height=180)
@@ -19,7 +22,7 @@ if run_button:
         tokens = lexer.generate_tokens()
 
         st.success("Análise léxica concluída com sucesso.")
-        st.dataframe(
+        df_streamlit = st.dataframe(
             [
                 {
                     "Token": token.type.name,
@@ -29,8 +32,24 @@ if run_button:
                     "Coluna": token.column,
                 }
                 for token in tokens
-            ],
-            width="stretch",
+            ]
         )
+
+        df_pandas = pd.DataFrame(
+            [
+                {
+                    "Token": token.type.name,
+                    "Descrição": token.type.value,
+                    "Lexema": token.value,
+                    "Linha": token.line,
+                    "Coluna": token.column,
+                }
+                for token in tokens
+            ]
+        )
+
+        csv = df_pandas.to_csv(index=False).encode("utf-8")
+
+        download_button = st.download_button(label="Baixar CSV", data=csv, file_name="data.csv", icon=":material/download:")
     except LexerError as error:
         st.error(str(error))
